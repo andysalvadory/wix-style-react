88d60cd4b4e363f7f9585fa56331fde7b3bee005
gendeld <danielge@wix.com>
2020-09-24 15:29:07 +0300

    Use useLayoutEffect instead of requestAnimationFrame

--- src/common/Ellipsis/Ellipsis.js
+++ src/common/Ellipsis/Ellipsis.js
@@ -1,31 +1,22 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import debounce from 'lodash/debounce';
 import shallowEqual from 'shallowequal';
-import requestAnimationFramePolyfill from '../../utils/request-animation-frame';
 import { st, classes, vars } from './Ellipsis.st.css';
 import Tooltip from '../../Tooltip';
 import { ZIndex } from '../../ZIndex';
 import { TooltipCommonProps } from '../PropTypes/TooltipCommon';
 
-/**
- *  This has been added in order to fix jsdom not having requestAnimation frame
- *  installed. Jest by default has this polyfilled, but mocha fails on it.
- */
-if (process.env.NODE_ENV === 'test') {
-  requestAnimationFramePolyfill.install();
-}
-
 const TextComponent = React.memo(
   React.forwardRef(({ render, ellipsis, maxLines, textDidUpdate }, ref) => {
+    useLayoutEffect(textDidUpdate);
+
     const getEllipsisClasses = () => {
       const ellipsisLines = maxLines > 1 ? 'multiline' : 'singleLine';
 
       return className =>
         ellipsis ? st(classes.text, { ellipsisLines }, className) : className;
     };
-
-    requestAnimationFrame(textDidUpdate);
 
     return render({
       ref,
