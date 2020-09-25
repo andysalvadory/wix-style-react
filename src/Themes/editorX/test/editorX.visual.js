496145a27bc0992d2df91f043800f179b67b6728
Shlomi Toussia-Cohen <shlomitc@wix.com>
2020-09-25 22:09:17 +0300

    minor

--- src/Themes/editorX/test/editorX.visual.js
+++ src/Themes/editorX/test/editorX.visual.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ThemeProvider from '../../ThemeProvider';
-import editorXTheme from './editorX';
-import { runTests as CustomModalLayoutTests } from '../../CustomModalLayout/test/CustomModalLayout.visual';
+import ThemeProvider from '../../../ThemeProvider';
+import editorXTheme from '../editorX';
+import { runTests as CustomModalLayoutTests } from '../../../CustomModalLayout/test/CustomModalLayout.visual';
 
 const themeName = 'Editor-X';
 const testWithTheme = test => {
