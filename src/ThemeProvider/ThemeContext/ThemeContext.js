91c90268341aa23415a38f102672a28239363408
Shlomi Toussia-Cohen <shlomitc@wix.com>
2020-09-25 22:22:30 +0300

    opt to the native context provider

--- src/ThemeProvider/ThemeContext/ThemeContext.js
+++ src/ThemeProvider/ThemeContext/ThemeContext.js
@@ -1,3 +1,3 @@
-import createReactContext from 'create-react-context';
+import React from 'react';
 
-export const ThemeContext = createReactContext();
+export const ThemeContext = React.createContext();
