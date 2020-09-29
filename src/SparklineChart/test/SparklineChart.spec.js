8e26c31730292a5fea4868f2369781733bb1ee96
OFIR <ofirb@wix.com>
2020-09-29 14:04:01 +0300

    fix unit test

--- src/SparklineChart/test/SparklineChart.spec.js
+++ src/SparklineChart/test/SparklineChart.spec.js
@@ -4,6 +4,12 @@ import { createRendererWithUniDriver, cleanup } from '../../../test/utils/unit';
 import SparklineChart from '../SparklineChart';
 import { sparklineChartPrivateDriverFactory } from './SparklineChart.private.uni.driver';
 
+const data = [
+  { label: new Date('Thu Sep 4 2020'), value: 3 },
+  { label: new Date('Thu Sep 5 2020'), value: 17 },
+  { label: new Date('Thu Sep 6 2020'), value: 18 },
+];
+
 describe(SparklineChart.displayName, () => {
   const render = createRendererWithUniDriver(
     sparklineChartPrivateDriverFactory,
@@ -12,7 +18,7 @@ describe(SparklineChart.displayName, () => {
   afterEach(cleanup);
 
   it('should render', async () => {
-    const { driver } = render(<SparklineChart />);
+    const { driver } = render(<SparklineChart data={data} />);
 
     expect(await driver.exists()).toBe(true);
   });
