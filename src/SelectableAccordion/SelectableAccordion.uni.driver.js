a3af4f693b69cefb98d0e3f7f4339dbf05f6cedd
TalZa <talza@wix.com>
2020-09-30 16:21:47 +0300

    item index

--- src/SelectableAccordion/SelectableAccordion.uni.driver.js
+++ src/SelectableAccordion/SelectableAccordion.uni.driver.js
@@ -10,11 +10,21 @@ export const selectableAccordionDriverFactory = (base, body) => {
   return {
     ...baseUniDriverFactory(base, body),
 
+    /**
+     * Clicks the item by item index
+     * @param {number} idx Item index
+     * @returns {Promise<void>} Title at item index
+     */
     clickItemAt: async idx => {
       const item = await getItemAt(base, idx);
       return findByHook(item, dataHooks.itemHeader).click();
     },
 
+    /**
+     * Checks whether the item is expanded by item index
+     * @param {number} idx Item index
+     * @returns {Promise<boolean>} Title at item index
+     */
     isItemExpandedAt: async idx => {
       const item = await getItemAt(base, idx);
       const itemState = await getDataAttributeValue(item, dataAttr.STATE);
